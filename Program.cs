a8941df5795d5529c91a2f305c3f4dd9f5de9908
Mathieu CARBONNEAUX <mathieu.carbonneaux@sfr.com>
2019-09-02 19:59:46 +0200

    fix cef ressource path search method

--- Program.cs
+++ Program.cs
@@ -18,47 +18,33 @@ namespace Web_Page_Screensaver
         [STAThread]
         static void Main(string[] args)
         {
-            //MessageBox.Show("Screensaver directory:"+ Environment.CurrentDirectory, "ScreenSaver", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            
-            //string szDir = "C:\\Donnees\\cefsharp";
-            string szDir = Environment.CurrentDirectory;
-            //Directory.SetCurrentDirectory(szDir);
-            
+            string szDir = Environment.CurrentDirectory; // get current directory
 
-            // Set version of embedded browser (http://weblog.west-wind.com/posts/2011/May/21/Web-Browser-Control-Specifying-the-IE-Version)
-            //var exeName = Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
-            //Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", exeName, 0x2AF8, RegistryValueKind.DWord);
+            // try to find processs directory where they are cef ressource
+            var szProcess = Process.GetCurrentProcess(); 
+            // get the directory name of the process fullpath filename
+            string szProcessusFullPath = Path.GetDirectoryName(szProcess.MainModule.FileName);
+
+            // check if libcef.dll are in this directory
+            if (File.Exists(szProcessusFullPath + "\\libcef.dll"))
+            {
+                szDir = szProcessusFullPath;
+                Directory.SetCurrentDirectory(szDir);
+            }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             
-            // https://docs.microsoft.com/fr-fr/windows/win32/sbscs/application-configuration-files#example
-            // https://docs.microsoft.com/fr-fr/cpp/build/adding-references-in-visual-cpp-projects?view=vs-2019
-            // https://github.com/dotnet/docs/blob/master/docs/framework/tools/gacutil-exe-gac-tool.md 
-
-            /*
- C:\Users\u038472\source\repos\web-page-screensaver\web-page-screensaver\bin\cefsharp>gacutil /il gac.txt
-Microsoft (R) .NET Global Assembly Cache Utility.  Version 4.0.30319.0
-Copyright (c) Microsoft Corporation. Tous droits réservés.
-
-Assembly C:\Users\u038472\source\repos\web-page-screensaver\web-page-screensaver\bin\cefsharp\CefSharp.BrowserSubprocess.Core.dll ajouté au cache.
-Assembly C:\Users\u038472\source\repos\web-page-screensaver\web-page-screensaver\bin\cefsharp\CefSharp.BrowserSubprocess.exe ajouté au cache.
-Assembly C:\Users\u038472\source\repos\web-page-screensaver\web-page-screensaver\bin\cefsharp\CefSharp.Core.dll ajouté au cache.
-Assembly C:\Users\u038472\source\repos\web-page-screensaver\web-page-screensaver\bin\cefsharp\CefSharp.dll ajouté au cache.
-Assembly C:\Users\u038472\source\repos\web-page-screensaver\web-page-screensaver\bin\cefsharp\CefSharp.WinForms.dll ajouté au cache.
-
-Nombre d'assemblys traités = 5
-Nombre d'assemblys installés = 5
-Nombre d'échecs = 0
-
-              */
             CefSettings settings = new CefSettings()
             {
                 //By default CefSharp will use an in-memory cache, you need to     specify a Cache Folder to persist data
                 CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache"),
+                // configure ressource path with screensave process directory path
                 ResourcesDirPath = szDir,
+                // configure browsersubprocess path based on screensaver directory
                 BrowserSubprocessPath = szDir + "\\CefSharp.BrowserSubprocess.exe",
+                // configure locales path based on screensaver directory
                 LocalesDirPath = szDir + "\\Locales\\",
                 
             };
@@ -67,7 +53,9 @@ Nombre d'échecs = 0
             Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
 
             if (args.Length > 0 && args[0].ToLower().Contains("/p"))
+            {
                 return;
+            }
 
             if (args.Length > 0 && args[0].ToLower().Contains("/c"))
             {
